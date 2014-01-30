a3604c4e535d4ea3eb082e1f0c10d42a8e03c1c6
Eric Betts <bettse@gmail.com>
2014-01-29 20:29:13 -0800

    reindenting, changing to cdnjs, updating bootstrap to 3.x

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,33 @@
+/* global $, console */
+
 var images = $.makeArray(document.querySelectorAll('.lolimage'));
 var pos = -301;
 var lazy = function() {
-    var indexes = [],
-        h = window.innerHeight + 100,
-        y = window.scrollY;
+  var indexes = [],
+  h = window.innerHeight + 100,
+  y = window.scrollY;
 
-    if(y - pos < 300 && !images.length) {
-        return;
+  if(y - pos < 300 && !images.length) {
+    return;
+  }
+  pos = y;
+
+  images.forEach(function(image, i) {
+    var top = image.getBoundingClientRect().top;
+    if(top<h) {
+      indexes.push(i);
     }
-    pos = y;
+  });
 
-    images.forEach(function(image, i) {
-        var top = image.getBoundingClientRect().top;
-        if(top<h) {
-            indexes.push(i);
-        }
-    });
-
-    indexes.sort(function(a, b){
-        return b > a;
-    }).forEach(function(n){
-        images[n].src = images[n].getAttribute('data-src');
-        console.log(n);
-        images.splice(n,1);
-    });
+  indexes.sort(function(a, b){
+    return b > a;
+  }).forEach(function(n){
+    images[n].src = images[n].getAttribute('data-src');
+    console.log(n);
+    images.splice(n,1);
+  });
 };
 $(function(){
-    lazy();
-    $(window).scroll(lazy);
+  lazy();
+  $(window).scroll(lazy);
 });
